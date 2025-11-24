24a62a13a72359f3097a8a7669dcafa7a93a0d48
mbskarthik5 <mbskarthik5@gmail.com>
2025-11-24 21:06:53 +0530

    Added variables code

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Learning!");
+        // தகவல்களை சேமித்தல்
+        String name = "Karthik"; 
+        int age = 25;
+
+        // திரையில் காட்டுதல்
+        System.out.println("My Name is: " + name);
+        System.out.println("My Age is: " + age);
     }
 }
