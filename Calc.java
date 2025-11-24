06d8ce44683741f6778e6b447e1c92355a2b65b7
mbskarthik5 <mbskarthik5@gmail.com>
2025-11-24 21:19:09 +0530

    Added Calculator code

---
+++ Calc.java
@@ -0,0 +1,15 @@
+class Calc {
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 50;
+
+        // கூட்டல் (Addition)
+        int sum = number1 + number2;
+
+        // கழித்தல் (Subtraction)
+        int diff = number1 - number2;
+
+        System.out.println("Total Sum is: " + sum);
+        System.out.println("Difference is: " + diff);
+    }
+}
